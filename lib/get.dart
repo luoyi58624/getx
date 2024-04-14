481227e4a08188bb34759b1c299d02c32dda5852
luoyi <luoyi58624@gmail.com>
2024-04-14 15:38:54 +0800

    删除getx http api

--- lib/get.dart
+++ lib/get.dart
@@ -5,7 +5,6 @@ library get;
 
 export 'get_animations/index.dart';
 export 'get_common/get_reset.dart';
-export 'get_connect/connect.dart';
 export 'get_core/get_core.dart';
 export 'get_instance/get_instance.dart';
 export 'get_navigation/get_navigation.dart';
